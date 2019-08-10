1fcad336c000c16cadd509266f64c4ba83f528a0
Scott W. Davis <zscottywd@msn.com>
2019-08-10 15:02:32 -0700

    Add files via upload

---
+++ src/BreakStatement.java
@@ -0,0 +1,12 @@
+public class BreakStatement {
+    public static void main(String[] args) {
+
+       for (int i = 1; i<=10 ; i++){
+           if (i == 5)
+               break;
+
+           System.out.println(i);
+       }
+        System.out.println("Out of for loop");
+    }
+}
